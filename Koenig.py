f5473878f9bbf738bb256322f3ab2f6b39758d61
Jacob Koglin <koglinja@b-tk-engy.logoip.de>
2026-02-11 13:22:41 +0100

    Fixed Rochade
    Signed-off-by: Jacob Koglin <koglinja@b-tk-engy.logoip.de>

--- Koenig.py
+++ Koenig.py
@@ -3,29 +3,29 @@ from sys import exit
 from FigurBuilder import FigurBuilder
 from Turm import Turm
 class Koenig(FigurBuilder):
-    def __init__(self, image:str, size:int, field_length:int, field_count:int, fieldLabelStartLetter:str, teamID:int, mustKill:bool=False):
-        super().__init__(image, size, field_length, field_count, fieldLabelStartLetter, teamID, True)
+    def __init__(self, image:str, size:int, field_length:int, feldanzahl:int, buchstabeFeldbezeichnung:str, teamID:int, mussSchlagen:bool=False):
+        super().__init__(image, size, field_length, feldanzahl, buchstabeFeldbezeichnung, teamID, True)
 
-        self.__mustKill:bool = mustKill
-        self.__canKill:bool = True
+        self.__mussSchlagen:bool = mussSchlagen
+        self.__kannSchlagen:bool = True
 
 
-        self.__hasAnxiety:bool = True
+        self.__hatAngst:bool = True
         self.setKingRole(True)
         
 
 
-    def getMaybePossibleTurns(self, originFieldLabel:str)->list[dict]:
-        possibleTurns = []
+    def getMaybePossibleTurns(self, vorherigesFeldBezeichnung:str)->list[dict]:
+        moeglicheZuege = []
         for i in range(-1, 2, 1):
             for j in range(-1, 2, 1):
                 if i == 0 and j == 0:
                     continue
-                possibleTurns = self.gebeNeueZuglisteMitNeuemZug(originFieldLabel, possibleTurns, (i, j), self.__mustKill, self.__canKill, self.__hasAnxiety)
+                moeglicheZuege = self.gebeNeueZuglisteMitNeuemZug(vorherigesFeldBezeichnung, moeglicheZuege, (i, j), self.__mussSchlagen, self.__kannSchlagen, self.__hatAngst)
         if not(self.getHasMoved()):
-            possibleTurns = self.gebeNeueZuglisteMitNeuemZug(originFieldLabel, possibleTurns, (2, 0), False, False, True, "castling", self.konvertiererelativenPunktzuFeldbezeichnung(originFieldLabel, (3, 0)), Turm, False, self.konvertiererelativenPunktzuFeldbezeichnung(originFieldLabel, (1, 0)))
-            possibleTurns = self.gebeNeueZuglisteMitNeuemZug(originFieldLabel, possibleTurns, (-2, 0), False, False, True, "castling", self.konvertiererelativenPunktzuFeldbezeichnung(originFieldLabel, (-3, 0)), Turm, False, self.konvertiererelativenPunktzuFeldbezeichnung(originFieldLabel, (-2, 0)))
-        return possibleTurns
+            moeglicheZuege = self.gebeNeueZuglisteMitNeuemZug(vorherigesFeldBezeichnung, moeglicheZuege, (2, 0), False, False, True, "castling", self.konvertiererelativenPunktzuFeldbezeichnung(vorherigesFeldBezeichnung, (3, 0)), Turm, False, self.konvertiererelativenPunktzuFeldbezeichnung(vorherigesFeldBezeichnung, (1, 0)))
+            moeglicheZuege = self.gebeNeueZuglisteMitNeuemZug(vorherigesFeldBezeichnung, moeglicheZuege, (-2, 0), False, False, True, "castling", self.konvertiererelativenPunktzuFeldbezeichnung(vorherigesFeldBezeichnung, (-3, 0)), Turm, False, self.konvertiererelativenPunktzuFeldbezeichnung(vorherigesFeldBezeichnung, (-2, 0)))
+        return moeglicheZuege
 
 
 
